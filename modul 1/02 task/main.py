a350b06859d3ef0b675d90ccd53dd1eb774c0e68
Andrey30011966 <dr.af2015@yandex.ru>
2024-09-18 08:06:03 +0300

    Выполнено

---
+++ modul 1/02 task/main.py
@@ -0,0 +1,6 @@
+count_homework = 12
+count_hours = 1.5
+course_name = 'Python'
+time_task = count_hours / count_homework
+
+print(f'Курс: {course_name}, всего задач: {count_homework}, затрачено часов: {count_hours}, среднее время выполнения {time_task} часа.')
